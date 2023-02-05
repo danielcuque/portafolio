651268732145a4a948d219c2f392b9d3d55d52bc
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2023-02-05 13:27:23 -0600

    first commit

---
+++ src/utils/navItems.ts
@@ -0,0 +1,19 @@
+interface NavItem {
+    name: string;
+    path: string;
+}
+
+export const navItems: NavItem[] = [
+    {
+        name: 'About',
+        path: '#about'
+    },
+    {
+        name: 'Projects',
+        path: '#projects'
+    },
+    {
+        name: 'Contact',
+        path: '#contact'
+    }
+]
