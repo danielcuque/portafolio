614ff3cebb7d9db28e257e5104113fbe09399514
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2023-02-07 23:31:50 -0600

    feat: fade-in animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,29 @@ module.exports = {
       fontFamily: {
         secondary: ['"Sofia Sans Extra Condensed"', "sans-serif"],
       },
+      colors: {
+        primary: {
+          50: "#EDC58B",
+          100: "FEBA34",
+          200: "#DF5904",
+          300: "#8E4F0A",
+        },
+        secondary: {
+          50: "#fc4b08",
+          100: "#1C626D",
+          200: "#1FACE7",
+          300: "#1A3176",
+        },
+      },
+      animation: {
+        fade: "fadeIn 1s ease-in-out",
+      },
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      }),
     },
   },
   plugins: [],
