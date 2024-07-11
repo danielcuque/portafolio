7642e84f04d6f366d0c7bf1259e35c64dfe2091d
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-07-11 14:04:26 -0600

    fix jobs

--- src/constants/jobs.ts
+++ src/constants/jobs.ts
@@ -7,12 +7,12 @@ export const jobs: IJob[] = [
         title: 'Full Stack Developer',
         location: 'Remote',
         startDate: new Date('2024-01-01:24:00:00Z'),
-        description: `I am currently working as a Full Stack Developer at Autority.
+        description: `I worked as a Full Stack Developer at Autority.
         
-        I am responsible for developing and maintaining the company\'s website and web applications.
+        I was responsible for developing and maintaining the company's website and web applications.
         
-        I also collaborate with other developers to implement new features and improve existing ones.
+        I also collaborated with other developers to implement new features and improve existing ones.
         
-        I use a variety of technologies, including TypeScript, Angular, Ionic, Node.js, and PostgreSQL.`,
+        I used a variety of technologies, including TypeScript, Angular, Ionic, Node.js, and PostgreSQL.`,
     }
 ]
